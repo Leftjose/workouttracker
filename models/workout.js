dc6fb8dc9f0793c29fed52b90974441af30440c4
Jose Izquierdo <leftjose@gmail.com>
2021-12-04 13:41:04 -0600

    adding models and node_modeuls folder

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongose
+
+
+
+
+excercise: {[
+    type: {
+        type: String,
+
+    }
+    name:{
+        type: String,
+    }
+    weight: {
+        type: String,
+    }
+    sets: {
+        type: String,
+
+    }
+    reps: {
+        type: String
+
+    }
+    duration: {
+        type: String 
+        
+
+    }
+]}
